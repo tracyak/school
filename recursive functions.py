b4eab2e8ecabbad8ddd1ab21d796fd6c4ec78e3e
tracyak <true.vania@gmail.com>
2025-02-27 06:46:50 +0000

    commit

--- recursive functions.py
+++ recursive functions.py
@@ -3,6 +3,8 @@ def factorial(n):
         return 1
     else:
        return n*factorial(n-1)
+    
+# print(factorial(5))
 
 myArray = [1,2,3,4,5,6,7,8,9,10]
 
@@ -13,16 +15,26 @@ def sumArray(myArray):
     else:
         return myArray[0] + sumArray(myArray[1:n])
 
-print(sumArray(myArray))
+# print(sumArray(myArray))
 
 # reversing a string
 
 def reverseStr(myString):
    n = len(myString)
-   newString = myString[n-1]
    if n == 1:
-        return myString
+        return myString[-1]
    else: 
-        return reverseStr(myString[n-1:1]) + newString
+        return myString[-1] + reverseStr(myString[:-1])
    
-print(reverseStr("tracy"))
+# print(reverseStr("tracy"))
+
+def letterCount(myString,letter):
+    n = len(myString)
+    if letter != myString[n-1]:
+        return 0
+    else:
+        return 1 + letterCount(myString[:-1],letter)
+    
+print(letterCount("aaa","a"))
+print(letterCount("tracy","y"))
+
