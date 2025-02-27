92322040a0fd70d96c0eb6d90f4bbd2b25591b65
tracyak <true.vania@gmail.com>
2025-02-27 01:59:19 +0000

    adfh

---
+++ recursive functions.py
@@ -0,0 +1,22 @@
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+       return n*factorial(n-1)
+
+import random
+myArray = [1,2,3,4,5,6,7,8,9,10]
+
+def sumArray():
+    sum = 0
+    if len(myArray) == 0:
+        return 0
+    else:
+        return sum + sumArray()
+
+print(sumArray)
+#reversing a string
+
+# def reverseStr(myString):
+#     n = len(myString):
+    
