49ec90f08183e33560a3390cb5c6b6cbd1ccecf6
tracyak <true.vania@gmail.com>
2025-04-24 01:27:22 +0000

    commit

--- Checkpoint Assessment 2025/Question1_PM24.py
+++ Checkpoint Assessment 2025/Question1_PM24.py
@@ -19,10 +19,22 @@ class Employee:
 AllEmployees = [] #1D array of 10 instances of Employee
 for i in range(10):
     try:
-        file = open("Employees.txt", 'r')
+        file = open("/workspaces/school/Checkpoint Assessment 2025/Employees.txt", 'r')
         Employee1 = Employee(file.readline(),int(file.readline()),file.readline())
         AllEmployees.append(Employee1)
     except FileNotFoundError:
         print("Error, file not found!")
 
-print(AllEmployees)
+
+EName = str(input("Please enter an employee's name:"))
+Found = False
+x = 0
+while not Found:
+    if Employee.GetName(AllEmployees[x]) == EName:
+        index = x
+        Found = True
+    else:
+        x += 1
+    if not Found and x == 10:
+        x = 0
+        EName = str(input("Employee name not found, Please enter a different employee's name:"))
